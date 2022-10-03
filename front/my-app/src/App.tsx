9be6bfb536984ac903918a0876739577a0dafc5f
Aleksandra Nedic <nedicaleksandra843@gmail.com>
2022-10-03 11:36:23 +0200

    Creating router and url paths

--- front/my-app/src/App.tsx
+++ front/my-app/src/App.tsx
@@ -1,26 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import HomePage from './HomePage/HomePage'
+import Navbar from './Navbar/Navbar'
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Router>
+            <Navbar />
+            <Routes>
+                <Route path="/" element={<HomePage/>} /> 
+            </Routes>
+        </Router>
+    );
 }
 
 export default App;
