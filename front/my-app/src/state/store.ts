c6a1223ddeca1c6839ba87f8c71e57f400e3e969
Aleksandra Nedic <nedicaleksandra843@gmail.com>
2022-10-12 10:36:24 +0200

    Add persisted store

--- front/my-app/src/state/store.ts
+++ front/my-app/src/state/store.ts
@@ -1,11 +1,22 @@
 import { configureStore } from "@reduxjs/toolkit";
+import storage from 'redux-persist/lib/storage';
+import { persistReducer, persistStore } from 'redux-persist';
 import logedUserReducer from "./logedUserSlice";
+
+const persistConfig = {
+  key: 'root',
+  storage,
+}
+const persistedReducer = persistReducer(persistConfig, logedUserReducer) as unknown as typeof logedUserReducer
+
 
 export const store = configureStore({
   reducer: {
-    logedUser: logedUserReducer,
+    logedUser: persistedReducer,
   },
 });
+
+export const persistor = persistStore(store)
 
 export type AppDispatch = typeof store.dispatch;
 export type RootState = ReturnType<typeof store.getState>;
